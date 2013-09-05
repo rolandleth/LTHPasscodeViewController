0cd8229697658d0b3bcaffdfae4c84239ef9cca4
Roland Leth <roland@Rolands-iMac.local>
2013-09-06 02:50:10 +0300

    Initial Commit

---
+++ LTHPasscodeViewController/LTHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LTHAppDelegate.h
+//  LTHPasscodeViewController
+//
+//  Created by Roland Leth on 9/6/13.
+//  Copyright (c) 2013 Roland Leth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LTHPasscodeViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LTHPasscodeViewController
+//
+//  Created by Roland Leth on 9/6/13.
+//  Copyright (c) 2013 Roland Leth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LTHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([LTHAppDelegate class]));
+	}
+}
