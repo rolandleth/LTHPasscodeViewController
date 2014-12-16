df2302b5434db7b2067820f828209e428f71836e
Julia Roggatz <julia_hro@gmx.de>
2014-12-16 16:29:57 +0100

    Reposition labels and entry field when not displayed as lock screen

--- LTHPasscodeViewController/LTHPasscodeViewController.m
+++ LTHPasscodeViewController/LTHPasscodeViewController.m
@@ -416,8 +416,8 @@ static const CGFloat kFailedAttemptLabelHeight = 22.0f;
 	// Modify this to fit your needs.
 	
 	
-	CGFloat verticalGap = is4InchDevice() ? kVerticalGap4Inch : kVerticalGap35Inch;
-	CGFloat topSpacing = is4InchDevice() ? kTopSpacing4Inch : kTopSpacing35Inch;
+	CGFloat verticalGap = is4InchDevice() ? kVerticalGap4Inch : self.displayedAsLockScreen ? kVerticalGap35Inch : kVerticalGap35Inch + 15.0f;
+	CGFloat topSpacing = is4InchDevice() ? kTopSpacing4Inch :self.displayedAsLockScreen ? kTopSpacing35Inch : kTopSpacing35Inch + 30.0f;
 	
 	NSLayoutConstraint *enterPasscodeConstraintCenterX =
     [NSLayoutConstraint constraintWithItem: _enterPasscodeLabel
