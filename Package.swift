b5d9b6845ebfc9de7801cd277ecba31f844725b0
Jason Rodriguez <jrodriguez@simplenexus.com>
2020-01-15 12:21:22 -0700

    Add Package.swift to library

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.2
+import PackageDescription
+let package = Package(
+    name: "LTHPasscodeViewController",
+    products: [
+        .library(name: "LTHPasscodeViewController", targets: ["LTHPasscodeViewController"])
+    ],
+    targets: [
+        .target(
+            name: "LTHPasscodeViewController",
+            dependencies: [],
+            path: ".",
+            exclude: ["Demo"],
+            sources: ["LTHPasscodeViewController", "Localizations"],
+        )
+    ]
+)
