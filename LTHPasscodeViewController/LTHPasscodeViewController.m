765f7c246a31602769cc0d1bdabe7f3fd5a0183d
Jason Rodriguez <jason.eli.rodriguez@gmail.com>
2021-12-06 09:56:46 -0700

    fixes SPM bundle issues

--- LTHPasscodeViewController/LTHPasscodeViewController.m
+++ LTHPasscodeViewController/LTHPasscodeViewController.m
@@ -20,7 +20,7 @@
 
 #ifndef LTHPasscodeViewControllerStrings
 #define LTHPasscodeViewControllerStrings(key) [LTHLocalizedString(key) length] == 0 \
-    ? [SWIFTPM_MODULE_BUNDLE localizedStringForKey:key value:@"" table:_localizationTableName] \
+    ? [[NSBundle bundleWithPath: [SWIFTPM_MODULE_BUNDLE pathForResource:@"LTHPasscodeViewController" ofType:@"bundle"]] localizedStringForKey:(@"Unlock using Touch ID") value:@"" table:_localizationTableName] \
     : LTHLocalizedString(key)
 #endif
 
