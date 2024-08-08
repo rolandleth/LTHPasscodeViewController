c2d74f3877f6ccb3156dbea86c9f9e2a690f476d
Joanda Febrian <132426322+mega-jfe@users.noreply.github.com>
2024-08-08 17:33:26 +0700

    T17249653: Fix incorrect color defaults when Design Token is toggled off

--- LTHPasscodeViewController/LTHPasscodeViewController.m
+++ LTHPasscodeViewController/LTHPasscodeViewController.m
@@ -1987,14 +1987,14 @@ static const NSInteger LTHMaxPasscodeDigits = 10;
     _backgroundColor = [UIColor mnz_mainBarsForTraitCollection:self.traitCollection];
     _passcodeBackgroundColor = [UIColor clearColor];
     _coverViewBackgroundColor = UIColor.systemBackgroundColor;
-    _failedAttemptLabelBackgroundColor =  UIColor.systemRedColor;
+    _failedAttemptLabelBackgroundColor = [UIColor clearColor];
     _enterPasscodeLabelBackgroundColor = [UIColor clearColor];
     _eraseLocalDataLabelBackgroundColor = [UIColor clearColor];
     
     // Text
     _labelTextColor = UIColor.labelColor;
     _passcodeTextColor = UIColor.labelColor;
-    _failedAttemptLabelTextColor = UIColor.systemBackgroundColor;
+    _failedAttemptLabelTextColor = UIColor.systemRedColor;
     _eraseLocalDataLabelTextColor = UIColor.systemRedColor;
     _optionsButtonTextColor = [UIColor colorWithRed:0 green:168.0/255.0 blue:134.0/255.0 alpha:1.0];
     _textFieldBorderColor = [UIColor colorWithRed:60.0/255.0 green:60.0/255.0 blue:67.0/255.0 alpha:0.3];
